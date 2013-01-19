dac51ef77f216c1ebac8970890bf1b32accc7aac
coskudemirhan <cosku@studionord.org>
2013-01-19 13:43:45 +0200

    First test

--- customizer.js
+++ customizer.js
@@ -1,3 +1,15 @@
+/******************************************************************************************************
+
+	jQuery.customizer
+
+	Author Coşku DEMİRHAN
+	Version 1.0
+	Date: 19/01/2013
+*/
+
 (function($) {
+	$.fn.customizer = function(c_obj, options) {
+		
+	}
 
 })(jQuery);
